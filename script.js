857c8540cfdb3c7fe22361e4fd6642088e45fd81
Jakob Füruter <jakobfueruter@MacBookPro.fritz.box>
2025-09-08 00:53:32 +0200

    Stand vor sharing mit Ben

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Klick-Button
-document.getElementById("clickMe").addEventListener("click", function() {
-  document.getElementById("message").textContent = "Du hast den Button geklickt!";
-});
 
-// Kontaktformular
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Danke für deine Nachricht!");
-});
